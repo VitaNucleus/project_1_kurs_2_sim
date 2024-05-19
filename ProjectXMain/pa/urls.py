83565d4c38d0603ffc06cb202164c30316ec9a2c
Daniil <127922540+VitaNucleus@users.noreply.github.com>
2024-05-19 17:05:04 +0500

    D Движемся к тому, чтобы отдавать данные аналитики

--- ProjectXMain/pa/urls.py
+++ ProjectXMain/pa/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .record_to_db import values_for_cian
-from .views import Lk, json_test, enter, Reg
+from .views import *
 
 
 urlpatterns = [
     path('home/', Lk, name='home'),
     path('test_json/', json_test, name='test_json'),
     path('values_for_cian/', values_for_cian, name='values_for_cian'),
+    path('test/', river_segment, name="river_segment")
 ]
--- ProjectXMain/pa/views.py
+++ ProjectXMain/pa/views.py
@@ -20,3 +20,23 @@ def json_test(request):
     with open(f"{dir}json_0.json", 'r') as file:
         object = json.load(file)
         return JsonResponse(object)
+
+
+def river_segment(request):
+    if request.method == 'POST':
+        min_area = float(request.POST.get('min_area', 50))
+        max_area = float(request.POST.get('max_area', 100))
+        min_price = float(request.POST.get('min_price', 0))
+        max_price = float(request.POST.get('max_price', 100000))
+        rent = request.POST.get('rent', False)
+
+        # selected_rivers = River.objects.filter(area__range=(min_area, max_area), price__range=(min_price, max_price))
+
+        # if rent:
+        #     selected_rivers = selected_rivers.filter(rent=True)
+        #
+        # average_price = selected_rivers.aggregate(avg_price=Avg('price'))['avg_price']
+
+        return render(request, 'result.html',) #{'average_price': average_price})
+
+    return render(request, 'river_segment.html')
